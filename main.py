46fb310b22a0bc499f634641faec291667ca90c6
Nathan Fritz <nbfritz@gmail.com>
2020-06-14 13:28:52 -0500

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+let strip = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
+strip.clear()
+strip.setBrightness(48)
+let hues = [0, 200, 160]
+let positions = [0, 1, 2]
+basic.forever(function () {
+    strip.clear()
+    for (let index = 0; index <= positions.length - 1; index++) {
+        positions[index] = (positions[index] + 1) % 24
+        strip.setPixelColor(positions[index], neopixel.hsl(hues[index], 100, 30))
+    }
+    strip.show()
+})
--- main.ts
+++ main.ts
@@ -1,3 +1,43 @@
+function setPositions (cnt: number) {
+    positions = []
+    for (let index = 0; index <= cnt - 1; index++) {
+        positions[index] = 24 / cnt * index
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    setHues(count)
+})
+function setHues (cnt: number) {
+    hues = []
+    for (let index = 0; index <= cnt - 1; index++) {
+        hues[index] = randint(0, 359)
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    setup()
+})
+function setup () {
+    count = randint(2, 7)
+    delay = count * 10 + 30
+    basic.showString("" + (delay))
+    setPositions(count)
+    setHues(count)
+}
+let delay = 0
+let hues: number[] = []
+let count = 0
+let positions: number[] = []
+let strip = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
+strip.clear()
+strip.setBrightness(48)
+setup()
 basic.forever(function () {
-	
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    for (let index = 0; index <= positions.length - 1; index++) {
+        positions[index] = (positions[index] + 1) % 24
+        hues[index] = (hues[index] + 2) % 360
+        strip.setPixelColor(positions[index], neopixel.hsl(hues[index], 100, 30))
+    }
+    strip.show()
+    basic.pause(delay)
 })
